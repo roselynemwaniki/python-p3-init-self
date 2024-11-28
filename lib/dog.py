6baee5d6efb4ed495bd24acb52c06332f36d2536
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-11-28 22:07:15 +0300

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+  def __init__(self, name, breed="Mutt"):
+        self.name = name  # Save the name to self.name
+        self.breed = breed  # Save the breed to self.breed (default is "Mutt")
